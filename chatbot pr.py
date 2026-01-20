18e8fb3726444be389477e24af420c74f0a2905d
shambhavidhole41-pixel <shambhavidhole41@gmail.com>
2026-01-20 10:48:04 +0530

    Add files via upload

---
+++ chatbot pr.py
@@ -0,0 +1,23 @@
+from operator import truediv
+
+
+       def chatbot (msg):
+           msg=msg.Lower()
+           elif "hi "in msg,"hello "in msg:
+           return "hello welcome to good luck cafe"
+       elif "menu" in msg:
+       return "tea,sandwitch,burger,coffee"
+elif "tea" ,in msg:
+return "lemon tea,green tea,special tea"
+elif "coffee" ,in msg:
+return "hot coffee,cold coffee"
+elif "sandwitch" , in msg:
+return "paneere sandwitch,grill sandwiotch"
+elif "burger", in msg
+return "spicy burger,sweet burger"
+elif "price", in msg:
+return "tea 20-30,coffee 80-100,burger 120-150,"
+elif "location", in msg:
+return "near by westside mall"
+
+while true (user)
